dca485665d96b0d1c6531313f3d54d72da69212d
Mandeep Mehra <mandeep.mehra@xebia.com>
2021-04-07 20:52:59 -0700

    Changed provider to local

--- provider.tf
+++ provider.tf
@@ -1,11 +1,4 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "tfstateepsilontraining3"
-    key    = "epsilon/mandeep/lambda/tfstate"
-    region = "us-east-1"
-  }
-}
