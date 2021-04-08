42097f95bfc5078bb53009c1316091f884c96900
Mandeep Mehra <mandeep.mehra@xebia.com>
2021-04-07 22:24:51 -0700

    Added S3 notification

--- main.tf
+++ main.tf
@@ -63,3 +63,25 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambdarole.name
   policy_arn = aws_iam_policy.lambda_logging.arn
 }
+
+
+# s3lambdatriggerexamplemandeep
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.myfunc.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.s3bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.s3bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.myfunc.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
